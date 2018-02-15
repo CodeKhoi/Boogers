3f66da20c59b1f23b4296d9a6cad204ecceb9b36
CodeKhoi <30888184+CodeKhoi@users.noreply.github.com>
2018-02-14 20:58:16 -0800

    Completed app wth JAWSDB

---
+++ config/connection.js
@@ -0,0 +1,28 @@
+// Set up MySQL connection.
+var mysql = require('mysql');
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+	  host: "localhost",
+	  user: "root",
+	  password: "",
+	  database: "burgers_db"
+	});
+};
+
+
+// Make the connection to MySQL
+connection.connect(function(err) {
+  if (err) {
+    console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
+    return;
+  }
+  console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
+});
+
+// Export connection for ORM use
+module.exports = connection;
